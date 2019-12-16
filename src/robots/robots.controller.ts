a02bb4962ba4df1025027f416652f59e2f7286e8
Michael Stifter <stifter@parkside.at>
2019-12-16 12:25:19 +0100

    refactor: remove create new robot endpoint

--- src/robots/robots.controller.ts
+++ src/robots/robots.controller.ts
@@ -1,14 +1,6 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Body,
-  NotFoundException,
-} from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
 import { Robot } from './robot.entity';
 import { RobotsService } from './robots.service';
-import { CreateRobotDto } from './create-robot.dto';
 
 @Controller('robots')
 export class RobotsController {
@@ -28,17 +20,5 @@ export class RobotsController {
     }
 
     return robot;
-  }
-
-  @Post()
-  async create(@Body() createRobotDto: CreateRobotDto): Promise<Robot> {
-    const newRobot = new Robot();
-    newRobot.name = createRobotDto.name;
-    newRobot.avatar = createRobotDto.avatar;
-    newRobot.experience = createRobotDto.experience;
-    newRobot.outOfOrder = createRobotDto.outOfOrder;
-    newRobot.powermove = createRobotDto.powermove;
-
-    return this.robotsService.create(newRobot);
   }
 }
