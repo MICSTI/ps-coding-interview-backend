b113fb43080c2284ac39466704f14a653b1f3b13
Michael Stifter <stifter@parkside.at>
2019-12-16 15:33:12 +0100

    build: switch from MySQL to Postgres

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from './shared/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-    }),
+    })
   );
-  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+
+  const apiBasePath = configService.get('API_BASE_PATH');
+  if (!apiBasePath) {
+    throw new InternalServerErrorException(
+      `environment variable API_BASE_PATH could not be resolved`
+    );
+  }
+  app.setGlobalPrefix(apiBasePath);
+
   await app.listen(3000);
 }
 bootstrap();
